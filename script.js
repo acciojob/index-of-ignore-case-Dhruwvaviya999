73cae8a8a9fe14c2128b65ed820ac9c393ca81d5
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-01-30 06:30:06 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
+	// write your code here
+
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+	for(let i=0; i<s1.length; i++){
+		if(s2[0] === s1[i]){
+			return i;
+		};
+	};
+};
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
